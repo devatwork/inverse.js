d1fcbacc9d69904fe9a375b1bcc04f9e3aaf636d
Bert Willems <b.willems@premotion.nl>
2014-01-02 15:47:29 +0100

    added a browser compatible build

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-mocha-test');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-gluejs');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
 	var srcFiles = [
 		'Gruntfile.js',
@@ -34,10 +36,33 @@ module.exports = function(grunt) {
 		watch: {
 			files: [srcFiles, testFiles],
 			tasks: 'check-code'
+		},
+		gluejs: {
+			browser: {
+				options: {
+					banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+					export: 'inversejs',
+					main: 'lib/index.js'
+				},
+				src: 'lib/**/*.js',
+				dest: 'dist/inverse.browser.js'
+			}
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+			},
+			browser: {
+				files: {
+					'dist/inverse.browser.min.js': ['dist/inverse.browser.js']
+				}
+			}
 		}
 	});
 
 	// Default task(s).
 	grunt.registerTask('check-code', ['jshint', 'mochaTest']);
 	grunt.registerTask('default', ['check-code']);
+	grunt.registerTask('browser', ['gluejs', 'uglify']);
+	grunt.registerTask('dist', ['check-code', 'browser']);
 };
